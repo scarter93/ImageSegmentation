7621e197765bdbd65f80bd29f4b50d73c97a8bee
Stephen Carter <stephen.carter@mail.mcgill.ca>
2016-04-04 19:50:53 -0400

    updated for VS2015 for my PC

---
+++ ImageSegmentation/generate_rect.cpp
@@ -0,0 +1,77 @@
+/**
+/ https://jayrambhia.wordpress.com/2012/09/20/roi-bounding-box-selection-of-mat-images-in-opencv/
+/
+/ Credit: Jay Rambhia
+/
+**/
+
+#include "generate_rect.h"
+
+
+
+Point point1, point2; /* vertical points of the bounding box */
+int drag = 0;
+Rect rect; /* bounding box */
+Mat img, roiImg; /* roiImg - the part of the image in the bounding box */
+int select_flag = 0;
+
+void mouseHandler(int event, int x, int y, int flags, void* param)
+{
+    if (event == CV_EVENT_LBUTTONDOWN && !drag)
+    {
+        /* left button clicked. ROI selection begins */
+        point1 = Point(x, y);
+        drag = 1;
+    }
+     
+    if (event == CV_EVENT_MOUSEMOVE && drag)
+    {
+        /* mouse dragged. ROI being selected */
+        Mat img1 = img.clone();
+        point2 = Point(x, y);
+        rectangle(img1, point1, point2, CV_RGB(255, 0, 0), 3, 8, 0);
+        imshow("image", img1);
+    }
+     
+    if (event == CV_EVENT_LBUTTONUP && drag)
+    {
+        point2 = Point(x, y);
+        rect = Rect(point1.x,point1.y,x-point1.x,y-point1.y);
+        drag = 0;
+        //roiImg = img(rect);
+    }
+     
+    if (event == CV_EVENT_LBUTTONUP)
+    {
+       /* ROI selected */
+        select_flag = 1;
+        drag = 0;
+    }
+}
+
+Rect generate_rect(Mat img_in){
+	img = img_in;
+	int k;
+
+	rectangle(img, rect, CV_RGB(255, 0, 0), 3, 8, 0);
+	cvSetMouseCallback("image", mouseHandler, NULL);
+	imshow("image", img);
+	while(1)
+    {
+		cvSetMouseCallback("image", mouseHandler, NULL);
+
+        k = waitKey(10);
+		
+        if (select_flag == 1)
+        {
+            break;
+        }
+    }
+	cvSetMouseCallback("image", NULL, NULL);
+	destroyWindow("image");
+	//rect = Rect(point1.x,point1.y,point2.x-point1.x,point2.y-point1.y);
+	std::cout << rect << endl;
+	return rect;
+}
+
+
---
+++ ImageSegmentation/main.cpp
@@ -0,0 +1,138 @@
+#include <string.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdint.h>
+#include "generate_rect.h"
+
+using namespace cv;
+//using namespace std;
+
+Mat my_kmeans(Mat &image, Rect &rectangle);
+Mat GraphCut(Mat &image, Rect &rectangle);
+
+int main(){
+	Mat image = imread("100_0109.png");
+	Rect rectangle = generate_rect(image);
+
+	if (rectangle.contains(Point(50, 50))) {
+		cout << "rectangle contains this point" << endl;
+	}
+
+	//imwrite("kmeans_out.png",my_kmeans(image, rectangle));
+	imwrite("graphCut_out.png", GraphCut(image, rectangle));
+	return 0;
+}
+
+Mat my_kmeans(Mat &image, Rect &rectangle){
+  
+    //std::string img(img_name);
+    //cout << "img_name = " << img_name << endl;
+    //cv::Mat image = cv::imread(img_name);
+	//imshow("test", image);
+	//waitKey(1);
+
+    Mat feature_mat = Mat::zeros(image.size().width*image.size().height,3,CV_32F);
+
+	//Mat feature_mat = image.clone().reshape(1,3).t();
+	//feature_mat.convertTo(feature_mat,CV_32F);
+	Mat gray_image;
+	cvtColor(image, gray_image, CV_BGR2GRAY);
+
+	for (int i = 0; i < image.rows; i++) {
+		for (int j = 0; j < image.cols; j++) {
+			if ( rectangle.contains(Point(j,i))) {
+				//cout << "inside rect" << endl;
+				feature_mat.at<float>(i*image.cols + j, 0) = image.at<cv::Vec3b>(i, j)[0];
+				feature_mat.at<float>(i*image.cols + j, 1) = image.at<cv::Vec3b>(i, j)[1];
+				feature_mat.at<float>(i*image.cols + j, 2) = image.at<cv::Vec3b>(i, j)[2];
+			}
+			else {
+				feature_mat.at<float>(i*image.cols + j, 0) = 0;
+				feature_mat.at<float>(i*image.cols + j, 1) = 0;
+				feature_mat.at<float>(i*image.cols + j, 2) = 0;
+
+			}
+		}
+	}
+
+	//for (int i = 0; i < gray_image.rows; i++) {
+	//	for (int j = 0; j < gray_image.cols; j++) {
+	//		if (rectangle.contains(Point(j, i))) {
+	//			//cout << "inside rect" << endl;
+	//			feature_mat.at<float>(i*gray_image.cols + j, 0) = gray_image.at<uchar>(i,j);
+	//			feature_mat.at<float>(i*gray_image.cols + j, 1) = gray_image.at<uchar>(i, j);
+	//			feature_mat.at<float>(i*gray_image.cols + j, 2) = gray_image.at<uchar>(i, j);
+	//		}
+	//		else {
+	//			feature_mat.at<float>(i*gray_image.cols + j, 0) = 0;
+	//			feature_mat.at<float>(i*gray_image.cols + j, 1) = 0;
+	//			feature_mat.at<float>(i*gray_image.cols + j, 2) = 0;
+
+	//		}
+	//	}
+	//}
+
+	//imwrite("pre kmeans.png", feature_mat);
+	//cout << feature_mat << endl;
+	
+	printf("feature_mat.rows,feature_mat.cols = %d,%d\n", feature_mat.rows,feature_mat.cols);
+	
+	Mat label = Mat::zeros(image.size().width*image.size().height,1, CV_32S);
+
+	for(int i = 0; i < image.rows; i++){
+		for( int j = 0; j < image.cols; j++){
+			if(rectangle.contains(Point(j,i))){
+				//cout << "point is located in rectangle" << endl;
+				label.at<int>(image.cols*i + j,0) = 1;	
+			}else{
+				label.at<int>(image.cols*i + j,0) = 0;
+			}
+		}
+	}
+
+	printf("finished copying labels\n");
+
+	Mat centers;
+    kmeans(feature_mat, 2, label,TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), 5, KMEANS_PP_CENTERS, centers);
+
+	cout << "generating feature matrix from kmeans function" << endl;
+	//feature_mat.reshape(image.size().width, image.size().height);
+	//imwrite("post kmeans.png", feature_mat);
+
+	//cout << feature_mat << endl;
+	//waitKey(1);
+
+	Mat foreground_mask = Mat::zeros(image.size().height,image.size().width, CV_8UC1);
+	cout << "generated and copying to foreground mask" << endl;
+	for(int i = 0; i < image.rows; i++){
+		for( int j = 0; j < image.cols; j++){
+			if(label.at<int>(i*image.cols + j,0) == 1){
+					foreground_mask.at<unsigned char>(i,j) = 255;
+			}
+		}
+	}
+	cout << "foreground mask copying complete" << endl;
+	imwrite("foreground_mask.png", foreground_mask);
+
+	Mat output;
+
+	image.copyTo(output, foreground_mask);
+
+	return output;
+    
+}
+
+Mat GraphCut(Mat &image, Rect &rectangle) {
+	Mat mask, bgdModel, fgdModel;
+	grabCut(image, mask, rectangle, bgdModel, fgdModel, 1, GC_INIT_WITH_RECT);
+
+	cv::compare(mask, cv::GC_PR_FGD, mask, cv::CMP_EQ);
+	//Mat foreground(image.size(), CV_8UC1, cv::Scalar(255));
+	imwrite("mask_GCM.png", mask);
+	Mat result;
+	image.copyTo(result, mask);
+
+	return result;
+
+}
